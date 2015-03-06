c55427ed51a6f2140d9ca2c17c0ac0d39b8d571b
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-06 05:09:14 -0500

    0.1.1

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*!
  * reflinks <https://github.com/jonschlinkert/reflinks>
  *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
